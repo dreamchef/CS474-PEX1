e7487ce93e3c194ef64e252631c775b835852327
Daniel Lisle <46765569+lys0l@users.noreply.github.com>
2021-02-01 21:55:28 -0700

    horizontal edge case handlers

--- draw.cpp
+++ draw.cpp
@@ -17,68 +17,74 @@ void draw(
 {
 	std::vector<vec4f> face;
 	face.resize(3);
+	float r, g, b;
+	float x0, x1, y;
+	float DeltaX0, DeltaX1;
 
 	// Copy faces into mutable vector
-
 	// Sort faces by depth
 
-	// Draw face
+	// DRAW FACE
 	for (int i = 0; i < scene.model.face.size(); i++) {
 
-		// Read color
-		float r = scene.model.faceColor.at((scene.model.face.at(i).at(0))).x*255;
-		float g = scene.model.faceColor.at((scene.model.face.at(i).at(0))).y*255;
-		float b = scene.model.faceColor.at((scene.model.face.at(i).at(0))).z*255;
+		// Read face color
+		r = scene.model.faceColor.at((scene.model.face.at(i).at(0))).x*255;
+		g = scene.model.faceColor.at((scene.model.face.at(i).at(0))).y*255;
+		b = scene.model.faceColor.at((scene.model.face.at(i).at(0))).z*255;
 
-		// ... sort indices of vertices
+		// Make copy of vertices vector
 		face.at(0) = scene.model.vertex.at(scene.model.face.at(i).at(0));
 		face.at(1) = scene.model.vertex.at(scene.model.face.at(i).at(1));
 		face.at(2) = scene.model.vertex.at(scene.model.face.at(i).at(2));
 
+		// Sort vertices by y-position
 		auto compareVertices = [](vec4f a, vec4f b) {
 			return (a.y > b.y);
 		};
+		std::sort(face.begin(), face.end(), compareVertices);
 
-		std::sort(face.begin(), face.end(), compareVertices); // Check correct order
-
+		// Convert to graphics window coordinate system
 		face.at(0).y = 800 - face.at(0).y;
 		face.at(1).y = 800 - face.at(1).y;
 		face.at(2).y = 800 - face.at(2).y;
 
-		// Calculate delta for longest side
-		float DeltaX0 = (face.at(0).x - face.at(2).x) / (face.at(0).y - face.at(2).y);
-
-		// Calculate second delta for upper half
-		float DeltaX1 = (face.at(0).x - face.at(1).x) / (face.at(0).y - face.at(1).y);
-
-		// HANDLE HORIZONTAL EDGE CASE
-
 		// Set starting scanline position
-		float x0 = face.at(0).x;
-		float x1 = face.at(0).x;
-		float y = face.at(0).y;
+		x0 = face.at(0).x;
+		x1 = face.at(0).x;
+		y = face.at(0).y;
 
-		// Draw upper triangle
-		while (y <= face.at(1).y) {
-			for(int x = min(x0,x1); x < max(x0,x1); x++)
-				SetPixelV(img, floor(x), floor(y), RGB(r, g, b));
-			x0 += DeltaX0;
-			x1 += DeltaX1;
-			y++;
+		// Set delta for longest side
+		DeltaX0 = (face.at(0).x - face.at(2).x) / (face.at(0).y - face.at(2).y);
+
+		if (face.at(0).y != face.at(1).y) { // Check for horizontal edge
+			// Set second delta for upper half
+			DeltaX1 = (face.at(0).x - face.at(1).x) / (face.at(0).y - face.at(1).y);
+			// Draw upper triangle
+			while (y < face.at(1).y) {
+				for (int x = min(x0, x1); x < max(x0, x1); x++)
+					SetPixelV(img, floor(x), floor(y), RGB(r, g, b));
+				x0 += DeltaX0;
+				x1 += DeltaX1;
+				y++;
+			}
 		}
-
-		// Update second delta for lower half
-		DeltaX1 = (face.at(1).x - face.at(2).x) / (face.at(1).y - face.at(2).y);
-
-		// Draw lower triangle
-		while (y <= face.at(2).y) {
-			for (int x = min(x0, x1); x < max(x0, x1); x++)
-				SetPixelV(img, floor(x), floor(y), RGB(r, g, b));
-			x0 += DeltaX0;
-			x1 += DeltaX1;
-			y++;
+		else {
+			x1 = face.at(1).x;
 		}
+		
+		if (face.at(1).y != face.at(2).y) { // Check for horizontal edge
+			// Set second delta for lower half
+			DeltaX1 = (face.at(1).x - face.at(2).x) / (face.at(1).y - face.at(2).y);
+			// Draw lower triangle
+			while (y < face.at(2).y) {
+				for (int x = min(x0, x1); x < max(x0, x1); x++)
+					SetPixelV(img, floor(x), floor(y), RGB(r, g, b));
+				x0 += DeltaX0;
+				x1 += DeltaX1;
+				y++;
+			}
+		}
+		
+		
 	}
-
-	//TODO: depth buffer, colors
 }
